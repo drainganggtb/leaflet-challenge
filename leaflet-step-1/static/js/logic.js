fe59d7191544bfd0572e55ea4b3956c71b5746c9
Justin <gree2167@umn.edu>
2021-01-29 17:54:42 -0600

    trying to get map to show up, data imports

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,74 +1,91 @@
+// Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-//perform GET request to query url
-d3.json(queryUrl, function(data) {
-    //once we get a response, send data to data.features object to createFeatures function
-    createFeatures(data.features);
-});
+//colorbrewer colors to be used in getColor function
+//// 
 
-function createFeatures(earthquakeData) {
-    //define a function we want to run once for each feature in the features array 
-    // give each feature a popup
-    function onEachFeature(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.title + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-        
+//function to determine color of markers
+function getColor(d) {
+    if (d < 1) {
+        color = '';
+    } else if (d <2) {
+        color= '';
+    } else if (d <3) {
+        color= '';
+    } else if (d <4) {
+        color= '';
+    } else if (d <5) {
+        color= '';
+    } else {
+        color '';
     }
-    
-    //create the GeoJSON layer containing features array on the earthquakeData object 
-    //Run the onEachFeature function once for each piece of data in the array
-    var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature
-    });
-    console.log(earthquakeData);
-    //send earthquakes layer to createMap function
-    createMap(earthquakes);
+    return color
 }
 
 function createMap(earthquakes) {
-    //define basemaps
-    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "outdoors/v-11",
-        accessToken: API_KEY
-    });
-    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        tileSize: 512,
-        zoomOffset: -1,
-        id: "dark-v10",
-        accessToken: API_KEY
-    });
-    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-        maxZoom: 18,
-        id: "satellite-streets-v11",
-        accessToken: API_KEY
-    });
 
-    //define basemaps object to hold layers
-    var baseMaps = {
-        "Outdoors": outdoors,
-        "Dark Map": darkmap,
-        "Satellite": satellite
-    };
+  // Define streetmap and darkmap layers
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
 
-    //create overlay object to hold overlay layer
-    var overlayMaps = {
-        Earthquakes: earthquakes
-    
-    
-    };
-    var myMap = L.map("mapid", {
-        center: [19.741755, -155.844437],
-        zoom: 5,
-        layers: [satellite, earthquakes]
-    });
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
 
-    //layer control
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-    }).addTo(myMap);
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Light Map": lightmap,
+    "Dark Map": darkmap
+  };
 
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    "Earthquakes": earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("mapid", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [darkmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
 }
+
+function createFeatures(earthquakeData) {
+    
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, createFeatures);
